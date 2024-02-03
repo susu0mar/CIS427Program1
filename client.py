8ffe636aa61a5102150337e0dab90563a87e5745
Souad <osouad@umich.edu>
2024-02-03 15:54:13 -0500

    Created basic client/server socket

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
 #This file is for all client operations
+import socket
+
+#creating socket object
+cs= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Chose random port number
+cs.connect((socket.gethostname(), 2323))
+
+message = cs.recv(1024).decode() #maximum of 1024 bytes can be recieved 
+
+print(f"Message from server: {message}")
+
+#close connection
+cs.close()
+
--- server.py
+++ server.py
@@ -1,1 +1,19 @@
 #This is file for all server operatons
+import socket
+
+#creating socket object which is ipv4 & uses TCP
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((socket.gethostname(), 2323))
+s.listen()
+
+#this loop runs until a connection is established
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection Successfully Established")#message to check if connection worked
+    #sending string to client
+    message_test = "Hello World! "
+    clientsocket.send(message_test.encode())
+
+    #close connection
+    clientsocket.close()
